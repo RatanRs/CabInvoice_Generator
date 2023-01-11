b418b6767ff0ead122cd6c39d360fcf0b42ddff6
9663461085 <ratansollapure16310@gmail.com>
2023-01-11 14:19:44 +0530

    Added:Calculating for multiple Rides

--- src/com/bridgelabzs/CabInvoiceGeneratorTest.java
+++ src/com/bridgelabzs/CabInvoiceGeneratorTest.java
@@ -5,9 +5,18 @@ import org.junit.Test;
 
 public class CabInvoiceGeneratorTest {
 	@Test
-	public void calculateFareOfOneRide() {
-		CabInvoiceGenarator Object = new CabInvoiceGenarator();
-		double test = Object.calculateFare(10, 15);
-		Assert.assertEquals(115.0, test, 0.0);
+	public void CalculateFareTest()  {
+		CabInvoiceGenarator obj=new CabInvoiceGenarator();
+		double test=obj.calculateFare(10, 15);
+		double test1=obj.calculateFare(10, 15)+obj.calculateFare(10, 15);;
+		Assert.assertEquals(230.0, test1, 0.0);
 	}
+	@Test
+	public void MonthFareTest()  {
+		CabInvoiceGenarator obj=new CabInvoiceGenarator();
+		TotalRide[] rides= { new TotalRide(1,23), new TotalRide(1,2)};
+		double monthfare=obj.calculateFare(rides);
+		Assert.assertEquals(45.0,monthfare , 0.0);
+		
+		}
 }
---
+++ src/com/bridgelabzs/TotalRide.java
@@ -0,0 +1,13 @@
+package com.bridgelabzs;
+
+public class TotalRide {
+	public final int time;
+	public double distance;
+
+	public TotalRide(double distance, int time) {
+		this.distance = distance;
+		this.time = time;
+	}
+}
+
+
